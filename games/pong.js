f62d468824172370226874d020ac2f24231c6bbe
unknown <ed.viagedor204@gmail.com>
2018-07-13 03:12:28 -0500

    Added a Paddle "superclass", paddles can now move concurrently, and added AnimationFrame

--- games/pong.js
+++ games/pong.js
@@ -1,14 +1,30 @@
+/**
+ * @file
+ * Pong!
+ * 
+ * @author edmund viagedor
+ */
+
 window.onload = function () {
+    "use strict";
 
-    const PADDLE_WIDTH = 10;
-    const PADDLE_HEIGHT = 65;
+    const PADDLE_WIDTH = 10, PADDLE_HEIGHT = 65;
+    let canvas = document.getElementById("canvas");
+    let context = canvas.getContext('2d');
+    let keysPressed = {}; // checks if a key is being held down or released; (https://stackoverflow.com/questions/1828613/check-if-a-key-is-down; answered by Robert)
 
-    var canvas = document.getElementById("canvas");
-    var context = canvas.getContext("2d");
-
-    context.fillStyle = "#ffffff";
-
+    /**
+     * Paddle object
+     */
     class Paddle {
+        /**
+         * @constructor
+         * @param {int} x 
+         * @param {int} y 
+         * @param {int} width 
+         * @param {int} height 
+         * @param {int} y_velocity 
+         */
         constructor(x, y, width, height, y_velocity) {
             this.x = x;
             this.y = y;
@@ -17,67 +33,175 @@ window.onload = function () {
             this.y_velocity = y_velocity;
         }
 
+        /**
+         * Draw the Paddle
+         */
+        render() {
+            context.fillStyle = "#ffffff";
+            context.fillRect(this.x, this.y, this.width, this.height);
+        }
+
+        /**
+         * Move the Paddle when its movement keys are pressed
+         * @param {int} y 
+         */
         move(y) {
             context.clearRect(this.x, this.y, this.width, this.height);
-
             this.y += y;
             this.y_velocity = y;
 
-            if(this.y > canvas.height) {
+            if (this.y < 0) { // Paddle at the top of canvas?
+                this.y = 0;
+                this.y_velocity = 0;
+            } else if (this.y > canvas.height - this.height) { // Paddle at the bottom of canvas?
                 this.y = canvas.height - this.height;
+                this.y_velocity = 0;
             }
         }
+    }
 
+    /**
+     * Player1's Paddle
+     */
+    class Player1 extends Paddle {
+        /**
+         * @constructor
+         * @param {int} x 
+         * @param {int} y 
+         * @param {int} width 
+         * @param {int} height 
+         * @param {int} y_velocity 
+         */
+        constructor(x, y, width, height, y_velocity) {
+            super(x, y, width, height, y_velocity);
+        }
+
+        /**
+         * Draws the Paddle on the canvas
+         */
         render() {
-            context.beginPath();
-            context.fillRect(this.x, this.y, this.width, this.height);
-            context.stroke();
+            super.render();
         }
 
-        get y() {
-            return this._y;
-        }
-
-        get y_velocity() {
-            return this._y_velocity;
-        }
-
-        set y(y) {
-            this._y = y;
-        }
-
-        set y_velocity(y_velocity) {
-            this._y_velocity = y_velocity;
+        /**
+         * Updates the y-coordinate of Player1
+         */
+        update() {
+            for (let key in keysPressed) {
+                let _key = Number(key); // convert the key to a number
+                
+                if (_key === 87) {
+                    super.move(-5);
+                } else if (_key === 83) {
+                    super.move(5);
+                }
+            }
         }
     }
 
-    class puck {
-        constructor(x, y, x_velocity, y_velocity) {
+    /**
+     * Player2's Paddle
+     */
+    class Player2 extends Paddle {
+        /**
+         * @constructor
+         * @param {int} x 
+         * @param {int} y 
+         * @param {int} width 
+         * @param {int} height 
+         * @param {int} y_velocity 
+         */
+        constructor(x, y, width, height, y_velocity) {
+            super(x, y, width, height, y_velocity);
+        }
+
+        /**
+         * Draws the Paddle on the canvas
+         */
+        render() {
+            super.render();
+        }
+
+        /**
+         * Updates the y-coordinate of Player2
+         */
+        update() {
+            for (let key in keysPressed) { // convert the key to a number
+                let _key = Number(key);
+
+                if (_key === 38) {
+                    super.move(-5);
+                } else if (_key === 40) {
+                    super.move(5);
+                }
+            }
+        }
+    }
+
+    /**
+     * Puck Object
+     */
+    class Puck {
+        /**
+         * @constructor
+         * @param {int} x the x coordinate 
+         * @param {int} y the y coordinate
+         */
+        constructor(x, y) {
             this.x = x;
             this.y = y;
-            this.x_velocity = x_velocity;
-            this.y_velocity = y_velocity;
+            this.x_velocity = 0;
+            this.y_velocity = 5;
+        }
+
+        /**
+         * Draws the Puck on the canvas
+         */
+        render() {
+            context.beginPath();
+            context.arc(this.x, this.y, 5, 2 * Math.PI, false);
+            context.fillStyle = "#ffffff";
+            context.fill();
+        }
+
+        update() {
+            
         }
     }
 
-    var paddle1 = new Paddle(5, 40, PADDLE_WIDTH, PADDLE_HEIGHT, 0);
-    var paddle2 = new Paddle((canvas.width - PADDLE_WIDTH) - 5, 200, PADDLE_WIDTH, PADDLE_HEIGHT, 0);
-    //var puck = new puck()
+    let player1 = new Player1(15, 40, PADDLE_WIDTH, PADDLE_HEIGHT, 0);
+    let player2 = new Player2((canvas.width - PADDLE_WIDTH) - 15, 200, PADDLE_WIDTH, PADDLE_HEIGHT, 10);
+    let puck = new Puck(canvas.width / 2, canvas.height / 2);
 
-    paddle1.render();
-    paddle2.render();
-
-    document.addEventListener('keydown', movePlayer);
-
-    function movePlayer(e) {
-        if (e.keyCode === 87) {
-            paddle1.move(-10);
-            paddle1.render();
-            console.log("MOVE PADDLE UP");
-        } else if (e.keyCode === 83) {
-            paddle1.move(10);
-            paddle1.render();
-            console.log("MOVE PADDLE DOWN");
-        }
+    function update() {
+        player1.update();
+        player2.update();
     }
+
+    function render() {
+        player1.render();
+        player2.render();
+        puck.render();
+    }
+
+    /**
+     * Performs a "game loop" with the constant calls from requestAnimationFrame
+     */
+    function show() {
+        update();
+        render();
+        window.requestAnimationFrame(show);
+    };
+
+    window.requestAnimationFrame(show);
+
+    // checks if a key is being held down or released
+    // (https://stackoverflow.com/questions/1828613/check-if-a-key-is-down; answered by Robert)
+    window.addEventListener("keydown", function (e) {
+        keysPressed[e.keyCode] = true;
+    }, false);
+
+    window.addEventListener("keyup", function (e) {
+        delete keysPressed[e.keyCode]; // remove the key from being a pressed key
+    }, false);
 }
